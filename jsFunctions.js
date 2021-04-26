8dcd9dba7f90d44d5132fae500febe161c14ebf1
Warren Harrison <warren@pdx.edu>
2021-04-26 15:49:34 -0700

    Added separate To Be Developed src link
    Moved the function stubs to a separate .js file - these are To Be Developed, and can be moved into the main .js file as they are completed

--- jsFunctions.js
+++ jsFunctions.js
@@ -55,20 +55,3 @@ function makeMove(direction)
 	checkStatus();
 }
 
-//checkStatus checks to see if the player is still viable
-//Does the player still have enough energy and supplies?
-//This should be checked after every move
-
-<!---->
-function checkStatus()
-{
-
-}
-
-//checkLocation checks to see if the player is still within the gameplay space
-//Student's choice on what to do if they left the galaxy - but something should happen
-
-<!---->
-function checkLocation()
-{
-}
